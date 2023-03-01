ebd4fdd53a3857aa897c0e0d76bb94fbd7de6d0c
AmandaCursino <125514189+AmandaCursino@users.noreply.github.com>
2023-03-01 15:14:38 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,9 @@
+n1 = float(input("Insira a primeira nota entre 0 e 100: "))
+n2 = float(input("Insira a segunda nota entre 0 e 100: "))
+
+nota_final = n1 + n2
+
+if nota_final >= 70 and nota1 >= 30 and nota2 >= 40:
+    print("O aluno foi aprovado.")
+else:
+    print("O aluno não foi aprovado.")
